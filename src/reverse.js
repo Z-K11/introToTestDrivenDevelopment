39981791ed23ed57fd60a7b1448058af8a2f5e0b
theZK11 <zulqarnainbazidkhail@gmail.com>
2026-07-30 15:32:13 +0500

    pass test for number reversed

--- src/reverse.js
+++ src/reverse.js
@@ -1,3 +1,10 @@
 export function reverseString(string) {
-  return 'olleh';
+  if (Number.isFinite(string)) {
+    string = String(string);
+  }
+  let reversed = [];
+  for (let j = string.length - 1; j >= 0; j--) {
+    reversed.push(string[j]);
+  }
+  return reversed.join('');
 }
--- src/reverse.test.js
+++ src/reverse.test.js
@@ -1,5 +1,10 @@
+import { ExternalsPlugin } from 'webpack';
 import { reverseString } from './reverse.js';
 
 test('Reverses a string', () => {
   expect(reverseString('hello')).toBe('olleh');
+});
+
+test('Converts a number into string and reverses it ', () => {
+  expect(reverseString(1234)).toBe('4321');
 });
